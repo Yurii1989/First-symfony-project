ae51f461eba2e4f78851919c954aa41aa8304902
yurii1989 <nlyuriinl@gmail.com>
2019-04-04 16:56:20 +0200

    registration form, basic concept for translations

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, ['label' => 'FORM.USER.USERNAME.LABEL'])
+            ->add('email', EmailType::class)
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'FORM.USER.PASSWORD.ERROR',
+                'first_options' => ['label' => 'FORM.USER.PASSWORD.LABEL'],
+                'second_options' => ['label' => 'FORM.USER.PASSWORD.LABEL2']
+            ])
+            ->add('termAccepted', CheckboxType::class, ['label' => 'FORM.USER.TOS.LABEL'])
+        ;
+
+        if ($options['standalone']) {
+            $builder->add('Submit', SubmitType::class);
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'standalone' => false
+        ]);
+    }
+}
